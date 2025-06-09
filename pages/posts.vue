4bd6c3121a8d4a4aa7962060bfd30faf66e80831
Abuzer Ansari <abuzeransari229@gmail.com>
2025-06-09 11:20:11 +0530

    Display posts using PostCard component and make them clickable

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-3xl font-bold">Posts Page</h1>
-  </div>
-</template>
-
-<script setup>
-</script>
+    <div class="p-6">
+      <h1 class="text-3xl font-bold mb-6">Posts Page</h1>
+  
+      <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <NuxtLink
+          v-for="post in posts"
+          :key="post.id"
+          :to="`/posts/${post.id}`"
+          class="block"
+        >
+          <PostCard :post="post" />
+        </NuxtLink>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { usePostsStore } from '~/stores/posts'
+  import PostCard from '~/components/PostCard.vue'
+  import { onMounted } from 'vue'
+  
+  const store = usePostsStore()
+  
+  onMounted(() => {
+    store.fetchPosts()
+  })
+  
+  const posts = store.posts
+  </script>
+  
